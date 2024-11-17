636573124f9c25a9aac765a9b3bc2b37e68150e2
toki <b45ti1i2s@gmail.com>
2024-11-17 17:38:21 +0900

    パラメータカスタマイズおよびプログレスバー

---
+++ DataTool_Data_DataLoupe/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CircularProgressBarExample
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private double _progressValue;
+        public double ProgressValue
+        {
+            get { return _progressValue; }
+            set
+            {
+                if (_progressValue != value)
+                {
+                    _progressValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- DataTool_Data_DataLoupe/MainWindow.xaml.cs
+++ DataTool_Data_DataLoupe/MainWindow.xaml.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using CircularProgressBarExample;
+using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
+using System.IO;
 
 namespace DataTool_Data_DataLoupe
 {
@@ -22,6 +27,11 @@ namespace DataTool_Data_DataLoupe
     public partial class MainWindow : Window
     {
         private DispatcherTimer timer;
+        private MainViewModel viewModel;
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,13 +41,21 @@ namespace DataTool_Data_DataLoupe
             timer.Tick += Timer_Tick;
             timer.Start();
 
+            viewModel = new MainViewModel();
+            DataContext = viewModel;
+        
         }
+                
+
+
         private void Timer_Tick(object sender, EventArgs e)
         {
             timer.Stop();
             startupImage.Visibility = Visibility.Collapsed;
             radialMenu.Visibility = Visibility.Visible;
             radialMenu.IsOpen = true;
+            progressbar.Visibility = Visibility.Visible;
+
         }
 
         private void CloseBotton(object sender, RoutedEventArgs e)
@@ -67,16 +85,58 @@ namespace DataTool_Data_DataLoupe
             MessageBox.Show("sample4");
         }
 
-        private void DriftButton(object sender, RoutedEventArgs e)
+        private async void DriftButton(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("sample5");
+
+            // プログレスバーを動かすためのタイマー
+            for (int i = 0; i <= 100; i++)
+            {
+                await Task.Delay(100); // 10秒待機
+                viewModel.ProgressValue = i;
+            }
+
+
+
         }
 
-        private void EvalutionButton(object sender, RoutedEventArgs e)
+        private async void EvalutionButton(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("sample6");
+
+            string relativeFilePath = @"Scripts\editor.bat"; // 実行したいバッチファイルのパス
+            string batchFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
+
+            try { 
+                ProcessStartInfo processStartInfo = new ProcessStartInfo(batchFilePath)
+                { 
+                    UseShellExecute = false, CreateNoWindow = true
+                }; 
+                Process process = new Process { StartInfo = processStartInfo }; 
+                process.Start();
+                await process.WaitForExitAsync(); // プロセスの終了を待機
+
+            } catch (Exception ex) 
+            { 
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error); 
+            }
+
+
+        }
+
+
+    }
+    public static class ProcessExtensions
+    {
+        public static Task WaitForExitAsync(this Process process)
+        {
+            if (process == null) throw new ArgumentNullException(nameof(process));
+            var tcs = new TaskCompletionSource<object>(); process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) => tcs.SetResult(null);
+            if (process.HasExited) tcs.SetResult(null);
+            return tcs.Task;
         }
     }
+
 
 
 }
